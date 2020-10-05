9e50fe516bbbb28d699a6bcd175c6504ba24cce3
Gert Braspenning <gert.braspenning@sap.com>
2020-10-05 20:14:54 +0200

    Add PE3 project

--- shepherd-implementation.js
+++ shepherd-implementation.js
@@ -1,32 +1,43 @@
-var tour;
-        
-tour = new Shepherd.Tour({
-    defaults: {
-        classes: 'shepherd-theme-arrows',
-        scrollTo: true
-    }
+const tour = new Shepherd.Tour({
+  defaultStepOptions: {
+    classes: 'class-1 class-2',
+    scrollTo: true
+  }
 });
 
-tour.addStep('example-step', {
-    text: 'This step is attached to the bottom of the element.',
-    attachTo: '.title bottom',
-    buttons: [
-      {
-        text: 'Next',
-        action: tour.next
-      }
-    ]
+tour.addStep({
+  id: 'example-step',
+  text: 'Welcome! Here you can see some work that I have done... :)',
+  classes: 'example-step-extra-class'
 });
 
-tour.addStep('example-step2', {
-    text: 'This step is attached to the bottom of the element.',
-    attachTo: '.title right',
-    buttons: [
-      {
-        text: 'Next',
-        action: tour.next
-      }
-    ]
+tour.addStep({
+  id: 'example-step3',
+  text: 'Do not forget to play the video!',
+  attachTo: {
+    element: ".tl-media-content",
+    on: 'bottom'
+  },
+  classes: 'example-step-extra-class'
+});
+
+tour.addStep({
+  id: 'example-step2',
+  text: 'You can navigate to the next project using this arrow!',
+  attachTo: {
+    element: ".tl-slidenav-icon",
+    on: 'left'
+  },
+  classes: 'example-step-extra-class'
 });
 
 tour.start();
+
+setTimeout(() => {
+  tour.next(); 
+  setTimeout(() => { 
+    tour.next(); 
+    setTimeout(() => tour.next(), 5000);
+  }, 5000);
+}, 5000);
+
--- timeline-implementation.js
+++ timeline-implementation.js
@@ -1,11 +1,35 @@
 timeline = new TL.Timeline('timeline-embed', {
     "events": [{
             "start_date": {
-                "year": 2012
+                "year": 2009,
+                "month": 4
             },
             "text": {
-                "headline": "test",
-                "text": "test"
+                "headline": "Java 2D Level Editor",
+                "text": "A student project for my first Java course!<br/><a href='https://github.com/brspnnggrt/private'>GitHub</a>"
+            },
+            "media": {
+                "url": "./projects/java/PE3_Editor.mp4",
+                "thumbnail": "./projects/java/thumbnail2.png"
+            },
+            "background": {
+                "url": "./projects/java/snippet.png"
             }
-    }]
+    },{
+        "start_date": {
+            "year": 2009,
+            "month": 5
+        },
+        "text": {
+            "headline": "Java 2D Role Playing Game",
+            "text": "Created a small game with my level editor"
+        },
+        "media": {
+            "url": "./projects/java/PE3_Game.mp4",
+            "thumbnail": "./projects/java/thumbnail.png"
+        },
+        "background": {
+            "url": "./projects/java/snippet.png"
+        }
+}]
 });
